917093b1514844a7d8579771ccccc391e77529d3
Artem Timkin <timkinartem84@gmail.com>
2026-08-22 19:34:11 +0300

    chore: initial commit, migrated from learning-journal monolith

---
+++ app.py
@@ -0,0 +1,111 @@
+import sqlite3
+
+from flask import Flask, render_template, url_for, redirect, request
+from datetime import date, datetime, timedelta
+from database import (
+    get_all_habits,
+    init_bd,
+    add_habit_to_db,
+    update_habit_in_db,
+    delete_habit_from_db,
+    get_stats,
+)
+from flask_login import LoginManager, UserMixin
+
+app = Flask(__name__)
+app.secret_key = "07052007"
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+init_bd()
+
+
+class User(UserMixin):
+    def __init__(self, id, email):
+        self.id = id
+        self.email = email
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = sqlite3.connect("habits.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, email FROM users WHERE id = ?", (user_id,))
+    user = cursor.fetchone()
+    conn.close()
+    if user:
+        return User(user[0], user[1])
+    return None
+
+
+# def load_habits():
+#     """Загружает привычки из JSON файла"""
+#     with open(JSON_PATH, 'r', encoding='utf-8') as f:
+#         return json.load(f)
+
+# def save_habits(habits):
+#     """Сохраняет привычки в JSON файл"""
+#     with open(JSON_PATH, 'w', encoding='utf-8') as f:
+#         json.dump(habits, f, ensure_ascii=False, indent=2)
+
+
+def calculate_new_streak(last_completed, current_streak):
+    """Принимает дату последнего выполнения и текущий streak."""
+    today = date.today()
+    last = datetime.strptime(last_completed, "%Y-%m-%d").date()
+
+    if last == today:
+        if current_streak == 0:
+            return 1, True
+        else:
+            return current_streak, False
+    elif last == today - timedelta(days=1):
+        return current_streak + 1, True
+    else:
+        return 1, True
+
+
+@app.route("/")
+def index():
+    """Главная страница со списком привычек"""
+    habits = get_all_habits()
+    stats = get_stats()
+    return render_template("index.html", habits=habits, stats=stats)
+
+
+@app.route("/complete/<int:habit_id>", methods=["POST"])
+def complete(habit_id):
+    """Отмечает привычку как выполненную"""
+    habits = get_all_habits()
+
+    # Ищем привычку по id, а не по индексу
+    for habit in habits:
+        if habit.get("id") == habit_id:
+            new_streak, should_update = calculate_new_streak(
+                habit["last_completed"], habit["streak"]
+            )
+            # habit['streak'] = habit.get('streak', 0) + 1
+            if should_update:
+                update_habit_in_db(habit_id, new_streak, date.today().isoformat())
+            else:
+                print("Уже выполнено сегодня")
+            break
+
+    return redirect(url_for("index"))
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+    add_habit_to_db(name)
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:habit_id>", methods=["POST"])
+def delete(habit_id):
+    delete_habit_from_db(habit_id)
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,115 @@
+import sqlite3
+from datetime import date
+
+DB_PATH = "habits.db"
+
+
+def get_all_habits():
+    """Возвращает список всех привычек из БД"""
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM habits")
+    rows = cursor.fetchall()
+    conn.close()
+
+    habits = []
+    for row in rows:
+        habit = {
+            "id": row[0],
+            "name": row[1],
+            "streak": row[2],
+            "last_completed": row[3],
+        }
+        habits.append(habit)
+
+    return habits
+
+
+def add_habit_to_db(name):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    today = date.today().isoformat()
+    conn.execute(
+        "INSERT INTO habits (name, last_completed) VALUES(?, ?)", (name, today)
+    )
+    new_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+    return new_id
+
+
+def init_bd():
+    """Создаёт таблицу habits, если её нет"""
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS habits (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            streak INTEGER DEFAULT 0,
+            last_completed TEXT NOT NULL
+        )
+    """
+    )
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+        )
+    """
+    )
+
+    conn.commit()
+    conn.close()
+    print("База данных готова!")
+
+
+def update_habit_in_db(habit_id, new_streak, new_date):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE habits SET streak = ?, last_completed = ? WHERE id = ?",
+        (new_streak, new_date, habit_id),
+    )
+    conn.commit()
+    conn.close()
+
+
+def delete_habit_from_db(habit_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM habits WHERE id = ?", (habit_id,))
+    conn.commit()
+    conn.close()
+
+
+def get_stats():
+    habits = get_all_habits()
+    cnt_habit = len(habits)
+    summa_streak = 0
+    max_streak = 0
+    max_habit_streak = None
+    for habit in habits:
+        summa_streak += habit["streak"]
+        if max_streak < habit["streak"]:
+            max_streak = habit["streak"]
+            max_habit_streak = habit["name"]
+
+    return {
+        "total_habits": cnt_habit,
+        "total_streak": summa_streak,
+        "best_streak": max_streak,
+        "best_habit": max_habit_streak,
+    }
+
+
+if __name__ == "__main__":
+    init_bd()
+    print(get_all_habits())
